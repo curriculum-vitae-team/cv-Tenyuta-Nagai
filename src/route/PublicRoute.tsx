ffff0e85b1231c249065b8162b7547abfe771c04
dimateniuta <dteniuta@gmail.com>
2023-01-16 20:14:26 +0300

    feat: add custom hook useAuth

--- src/route/PublicRoute.tsx
+++ src/route/PublicRoute.tsx
@@ -1,15 +1,14 @@
 import React, { FC } from 'react';
 import { Navigate } from 'react-router-dom';
-import { useReactiveVar } from '@apollo/client';
 import { RoutePath } from '../constants/routeVariables';
-import { authService } from '../graphql/authentication/authService';
+import { useAuth } from '../hooks/useAuth';
 
 interface IPublicRouteProps {
   children: React.ReactNode | JSX.Element;
 }
 
 export const PublicRoute: FC<IPublicRouteProps> = ({ children }) => {
-  const isAuth = useReactiveVar(authService.access_token$);
+  const { isAuth } = useAuth();
 
   if (isAuth) {
     return <Navigate to={RoutePath.EMPLOYEES} replace />;
