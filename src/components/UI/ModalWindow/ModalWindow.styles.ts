41b8a3b20b60d532338b12297e1c7322928313cc
dimateniuta <dteniuta@gmail.com>
2023-01-20 11:55:53 +0300

    refactor: remove unnecessary tyles

--- src/components/UI/ModalWindow/ModalWindow.styles.ts
+++ src/components/UI/ModalWindow/ModalWindow.styles.ts
@@ -4,11 +4,11 @@ export const WrapperTheme = styled(Dialog)(({ theme }) => ({
   '& .MuiDialogContent-root': {
     padding: theme.spacing(2),
   },
-})) as typeof Dialog;
+}));
 
 export const IconButtonModalWindow = styled(IconButton)(({ theme }) => ({
   position: 'absolute',
   right: 8,
   top: 8,
   color: theme.palette.secondary.contrastText,
-})) as typeof IconButton;
+}));
