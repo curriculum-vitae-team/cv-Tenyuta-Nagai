a239f1b430b184366a9829be02a0ced7db6cc548
darnnng <dar.nagaj@gmail.com>
2023-02-13 16:12:21 +0300

    feat: added refetch on employees

--- src/components/pages/EmployeesPage/EmployeesModal/EmployeesModal.tsx
+++ src/components/pages/EmployeesPage/EmployeesModal/EmployeesModal.tsx
@@ -14,11 +14,14 @@ import { InputText } from '../../../UI/InputText';
 import { CreateUserResult, IUserAllResult } from '../../../../graphql/types/results/user';
 import { modalService } from '../../../../graphql/service/modalService';
 import { ModalWindowButton } from '../../../UI/ModalWindowButton';
+import { GET_ALL_USERS } from '../../../../graphql/queries/users';
 import { IEmployeesFormInput } from './EmployeesModal.interface';
 
 export const EmployeesModal = () => {
   const { loading, positionsData, departmentsData, rolesData } = useEmployeesFormData();
-  const [createUser, { loading: updateLoading }] = useMutation<IUserAllResult>(CREATE_USER);
+  const [createUser, { loading: updateLoading }] = useMutation<IUserAllResult>(CREATE_USER, {
+    refetchQueries: [{ query: GET_ALL_USERS }, 'getUsers'],
+  });
   const {
     register,
     handleSubmit,
