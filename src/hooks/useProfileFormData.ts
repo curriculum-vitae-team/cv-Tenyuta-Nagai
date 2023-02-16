68e785fdb15eff584f22f5e1b3549138d9f8459d
darnnng <dar.nagaj@gmail.com>
2023-02-16 12:52:46 +0300

    fix: deleted unnecessary from hook

--- src/hooks/useProfileFormData.ts
+++ src/hooks/useProfileFormData.ts
@@ -1,13 +1,10 @@
 import { useQuery } from '@apollo/client';
 import { DEPARTMENTS } from '../graphql/queries/departments';
 import { POSITIONS } from '../graphql/queries/positions';
-import { SKILLS } from '../graphql/queries/skills';
 import { USER } from '../graphql/queries/user';
 import { IDepartmentReturn } from '../graphql/types/results/department';
 import { IPositionReturn } from '../graphql/types/results/position';
-import { ISkillsReturn } from '../graphql/types/results/skills';
 import { IUserAllResult } from '../graphql/types/results/user';
-import { SkillsMastery } from '../components/pages/EmployeesSkillsPage/constants/skillsMastery';
 
 export const useProfileFormData = (id: string) => {
   const { loading: loadingUser, error: errorUser, data: userData } = useQuery<IUserAllResult>(
@@ -22,22 +19,12 @@ export const useProfileFormData = (id: string) => {
   const { loading: positionsLoading, error: positionsError, data: positionsData } = useQuery<
     IPositionReturn
   >(POSITIONS);
-  const { loading: skillsLoading, error: skillsError, data: skillsData } = useQuery<ISkillsReturn>(
-    SKILLS
-  );
-  const skillMasteryData = [
-    { id: SkillsMastery.Novice, name: SkillsMastery.Novice },
-    { id: SkillsMastery.Advanced, name: SkillsMastery.Advanced },
-    { id: SkillsMastery.Expert, name: SkillsMastery.Expert },
-  ];
 
   return {
-    loading: loadingUser || departmentsLoading || positionsLoading || skillsLoading,
-    error: errorUser || departmentsError || positionsError || skillsError,
+    loading: loadingUser || departmentsLoading || positionsLoading,
+    error: errorUser || departmentsError || positionsError,
     userData,
     departmentsData,
     positionsData,
-    skillsData,
-    skillMasteryData,
   };
 };
